aef4629aaec1c515a704a51ce411f89721b097e3
Denis Juan Alonso Glez <denjalonso@gmail.com>
2024-08-10 13:22:21 +0100

    Type safe request validations

--- server/src/apps/profile/backend/routes/users.route.ts
+++ server/src/apps/profile/backend/routes/users.route.ts
@@ -1,7 +1,14 @@
 import { Router, Request, Response } from 'express';
 import container from '../dependency-injection';
+import { body } from 'express-validator';
+import { validateReqSchema } from './index';
 
 export const register = (router: Router) => {
+	// Those validations are communication protocol validations, not value object validations
+	const reqSchema = [body('id').exists().isString(), body('name').exists().isString()];
+
 	const userPutController = container.get('Apps.profile.controllers.UserPutController');
-	router.put('/users/:id', (req: Request, res: Response) => userPutController.run(req, res));
+	router.put('/users/:id', reqSchema, validateReqSchema, (req: Request, res: Response) =>
+		userPutController.run(req, res)
+	);
 };
--- server/tests/apps/profile/backend/features/users/create-user.feature
+++ server/tests/apps/profile/backend/features/users/create-user.feature
@@ -7,8 +7,20 @@ Feature: Create new user
     Given I send a PUT request to "/users/ef8ac118-8d7f-49cc-abec-78e0d05af80a" with body:
     """
     {
+      "id": "ef8ac118-8d7f-49cc-abec-78e0d05af80a",
       "name": "Parrot"
     }
     """
     Then the response status code should be 201
     And the response should be empty
+
+  Scenario: A invalid non existing user
+    Given I send a PUT request to "/users/ef8ac118-8d7f-49cc-abec-78e0d05af80a" with body:
+    """
+    {
+      "id": "ef8ac118-8d7f-49cc-abec-78e0d05af80a",
+      "name": 5
+    }
+    """
+    Then the response status code should be 422
+    # 422 Unprocessable Entity

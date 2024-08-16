2f41428ddfa014d1f01ada4b595eb9a2e71e7831
Denis Juan Alonso Glez <denjalonso@gmail.com>
2024-08-16 13:00:16 +0100

    Configs

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,7 +1,6 @@
 export default {
   testEnvironment: 'jest-environment-jsdom', // Same name of the lib you installed
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'], // The file you created to extend jest config and "implement" the jest-dom environment in the jest globals
-  setupFiles: [require.resolve('whatwg-fetch')],
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/test/__mocks__/fileMock.js', // The global stub for weird files
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy', // The mock for style related files
